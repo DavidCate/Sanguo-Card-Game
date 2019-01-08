45b35af13f55e83a754f0f03ec3230de85d8a5fe
DavidCate <1049698300@qq.com>
2019-01-08 17:11:10 +0800

    fastdfs module

---
+++ Sanguo-FastDFS-Server/src/test/java/spingboot/MyTest.java
@@ -0,0 +1,22 @@
+package spingboot;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MyTest {
+
+
+
+    @Test
+    public void test() {
+
+    }
+
+
+}
