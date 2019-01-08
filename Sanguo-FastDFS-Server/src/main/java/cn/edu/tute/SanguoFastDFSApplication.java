849f57510e2a9ee87e6a92138f140d79fbc44c98
DavidCate <1049698300@qq.com>
2019-01-08 17:10:03 +0800

    fastdfs module

---
+++ Sanguo-FastDFS-Server/src/main/java/cn/edu/tute/SanguoFastDFSApplication.java
@@ -0,0 +1,11 @@
+package cn.edu.tute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SanguoFastDFSApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SanguoFastDFSApplication.class,args);
+    }
+}
