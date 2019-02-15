38c4130e09ea03db23cf098f278b358b27dc8f84
DavidCate <1049698300@qq.com>
2019-02-15 15:02:05 +0800

    ES

--- Sanguo-ES-Server/src/main/java/cn/edu/tute/server/controller/ESController.java
+++ Sanguo-ES-Server/src/main/java/cn/edu/tute/server/controller/ESController.java
@@ -6,9 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.stereotype.Controller;
 
-
-import java.io.IOException;
-
 @Controller
 public class ESController {
     @Autowired
