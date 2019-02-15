33b65d9f0ecac87f722e36f9fb81d493da5360ce
DavidCate <1049698300@qq.com>
2019-02-15 12:14:01 +0800

    添加了存储文件信息

--- Sanguo-FastDFS-Server/src/main/java/cn/edu/tute/controller/FileUpDownControll.java
+++ Sanguo-FastDFS-Server/src/main/java/cn/edu/tute/controller/FileUpDownControll.java
@@ -3,24 +3,36 @@ package cn.edu.tute.controller;
 import cn.edu.tute.entities.Constans;
 import cn.edu.tute.fastdfs.FastDFSUtils;
 import cn.edu.tute.fastdfs.UploadFileVo;
+import cn.edu.tute.service.FileStorageService;
 import com.alibaba.fastjson.JSONObject;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 
 @Controller
 public class FileUpDownControll {
     Logger logger= LoggerFactory.getLogger(FileUpDownControll.class);
 
+    @Autowired
+    FileStorageService fileStorageService;
+
     @RequestMapping("upload/uploadPic")
-    public void uploadFile(MultipartFile pic, HttpServletResponse response){
+    public void uploadFile(MultipartFile pic, HttpServletResponse response, HttpServletRequest request){
+        HttpSession httpSession=request.getSession();
+        int userId=(Integer) httpSession.getAttribute("");
         UploadFileVo uploadFileVo=new UploadFileVo();
         try {
             uploadFileVo.setFileBytes(pic.getBytes());
@@ -32,6 +44,9 @@ public class FileUpDownControll {
             String url= Constans.FDFS_URL+"/"+path;
             JSONObject jsonObject=new JSONObject();
             if (StringUtils.isNotBlank(path)){
+                Date now=new Date();
+                SimpleDateFormat simpleDateFormat=new SimpleDateFormat("yyyy年MM月dd日 hh时mm分ss秒 EE", Locale.CHINA);
+                fileStorageService.fileStorage(uploadFileVo.getFileName(),url,simpleDateFormat.format(now),userId);
                 jsonObject.put("url",url);
                 jsonObject.put("isSuccess",true);
             }else {
